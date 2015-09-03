c7296a1e761c318ae90f4d2156a5fadde78f72f0
Ward W. Vuillemot <wwv@mac.com>
2015-09-02 20:24:16 -0500

    Refactor external links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     path == request.path
   end
 
+  def link_to_github
+    'http://github.com/wwvuillemot/Profile'
+  end
+
+  def link_to_linkedin
+    'http://linkedin.com/in/wwvuillemot'
+  end
+
+  def link_to_wardspics
+    'http://wardspics.com'
+  end
+
 end
